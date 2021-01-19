b12f0c680d44b32d342083a1808508c641f68ba8
Karolina <karolinamartamaj@gmail.com>
2021-01-19 14:46:08 +0100

    scraper green dreamer

---
+++ tutorials/tutorials/spiders/green_dreamer.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class PostSpider(scrapy.Spider):
+    name = "ngo_posts"
+    start_urls = [
+        'https://greendreamer.com/journal/environmental-organizations-nonprofits-for-a-sustainable-future'
+        # w tym przypakdu mamy tylko jedną stronę
+    ]
+
+    def parse(self, response):
+        divs = response.css('div.sqs-block-content')
+        for div in divs:
+            for h in div.css("h3"):
+                print(h.xpath("./text()").extract())
+                NGOname = h.xpath("./text()").extract()  # nazwa organizacji
+                NGOdescrpit = h.xpath('following-sibling::p[1]/text()').extract()  # opis organizacji
+                p = h.xpath('following-sibling::p[1]')
+                if p.css('a') != []: #  nie chcemy fragmentu 'side note:'
+                    print(h.xpath('following-sibling::p[1]/text()').extract())
+                    NGOlink = p.css('a').attrib['href']  # adres organizacji
+                    print(p.css('a').attrib['href'])
+
+                # print(p.get()) - zwraca to samo co wyzej tylko z tagami i z <a>
+                # zwrocenie get involved całęgo (razem z linkami) plus poprawa bo side note sie wywala
+
